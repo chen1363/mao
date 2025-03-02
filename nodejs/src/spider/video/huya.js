a73e878ba3d1183079f89f55d37c625f6386818b
hrsyr <66700988+chen1363@users.noreply.github.com>
2025-03-03 01:24:29 +0800

    Update huya.js

--- nodejs/src/spider/video/huya.js
+++ nodejs/src/spider/video/huya.js
@@ -372,7 +372,7 @@ async function test(inReq, outResp) {
 export default {
     meta: {
         key: 'huya',
-        name: '🟢 虎牙',
+        name: '虎牙',
         type: 3,
     },
     api: async (fastify) => {
