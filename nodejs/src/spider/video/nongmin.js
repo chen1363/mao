b844697983293813dd9e44144094abd6999f5525
hrsyr <66700988+chen1363@users.noreply.github.com>
2025-03-03 01:32:34 +0800

    Update nongmin.js

--- nodejs/src/spider/video/nongmin.js
+++ nodejs/src/spider/video/nongmin.js
@@ -544,7 +544,7 @@ async function test(inReq, outResp) {
 export default {
     meta: {
         key: 'nongmin',
-        name: '🟢 农民',
+        name: '农民',
         type: 3,
     },
     api: async(fastify) => {
