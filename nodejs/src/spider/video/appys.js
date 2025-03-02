4cb3bd6edf3b5f1a9160b02b82afe788c928779d
hrsyr <66700988+chen1363@users.noreply.github.com>
2025-03-03 01:15:42 +0800

    Update appys.js

--- nodejs/src/spider/video/appys.js
+++ nodejs/src/spider/video/appys.js
@@ -926,7 +926,7 @@ async function test(inReq, outResp) {
 export default {
     meta: {
         key: 'appys',
-        name: '🟢 聚合',
+        name: 'app聚合',
         type: 3,
     },
     api: async (fastify) => {
